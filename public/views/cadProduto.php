7e6a5527b893c8111bcd46a97c78c76a7e64bdfe
Mateus Soares da Rocha Cordeiro <msgdhdjf777@gmail.com>
2024-08-04 16:17:37 -0300

    conserto do css

--- public/views/cadProduto.php
+++ public/views/cadProduto.php
@@ -25,18 +25,36 @@ if (!isset($_SESSION['login'])) {
     <main>
         <div class="cad-container">
             <form action="" method="post" enctype="multipart/form-data">
-                <label for="nome">Nome do produto</label>
-                <input type="text" name="nome" id="nome" required>
-                <label for="descricao">Descrição do produto</label>
-                <textarea name="descricao" id="descricao" required></textarea>
-                <label for="preco">Preço</label>
-                <input type="number" name="preco" id="preco" step="0.01" required>
-                <label for="img">Imagem do produto</label>
-                <input type="file" name="img" id="img" accept="image/*" required>
-                <label for="quantidade">Quantidade disponivel</label>
-                <input type="number" name="quantidade" id="quantidade" min="1" required>
-                <label for="fornecedor">Fornecedor do produto</label>
-                <input type="text" name="fornecedor" id="fornecedor" required>
+                <div class="caixa-input">
+                    <div class="caixa">
+                        <label for="nome">Nome do produto</label>
+                        <input type="text" name="nome" id="nome" required>
+                    </div>
+                    <div class="caixa">
+                        <label for="descricao">Descrição do produto</label>
+                        <textarea name="descricao" id="descricao" required></textarea>
+                    </div>
+                </div>
+                <div class="caixa-input">
+                    <div class="caixa">
+                        <label for="preco">Preço</label>
+                        <input type="number" name="preco" id="preco" step="0.01" required>
+                    </div>
+                    <div class="caixa">
+                        <label for="img">Imagem do produto</label>
+                        <input type="file" name="img" id="img" accept="image/*" required>
+                    </div>
+                </div>
+                <div class="caixa-input">
+                    <div class="caixa">
+                        <label for="quantidade">Quantidade disponivel</label>
+                        <input type="number" name="quantidade" id="quantidade" min="1" required>
+                    </div>
+                    <div class="caixa">
+                        <label for="fornecedor">Fornecedor do produto</label>
+                        <input type="text" name="fornecedor" id="fornecedor" required>
+                    </div>
+                </div>
                 <input type="submit" value="Cadastrar" name="cadProduto">
             </form>
         </div>
